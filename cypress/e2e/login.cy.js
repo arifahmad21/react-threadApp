ddfa1981ce2f1f8023105fca1230c5d6470a3096
arifahmad21 <arifahmad0203@gmail.com>
2024-06-18 18:28:21 +0700

    make test pass

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,7 @@
  *   - should display alert when username is empty
  *   - should display alert when password is empty
  *   - should display alert when username and password are wrong
+ *   - should display homepage when username and password are correct
  */
 
 describe('Login spec', () => {
@@ -28,7 +29,7 @@ describe('Login spec', () => {
   });
 
   it('should display alert when password is empty', () => {
-    cy.get('input[placeholder="Email"]').type('tryariff@gmail.com');
+    cy.get('input[placeholder="Email"]').type('testariff@gmail.com');
 
     cy.get('button').contains(/^Login$/).click();
 
@@ -38,7 +39,7 @@ describe('Login spec', () => {
   });
 
   it('should display alert when email and password are wrong', () => {
-    cy.get('input[placeholder="Email"]').type('tryariff@gmail.com');
+    cy.get('input[placeholder="Email"]').type('testariff@gmail.com');
 
     cy.get('input[placeholder="Password"]').type('wrongpass');
 
@@ -47,5 +48,16 @@ describe('Login spec', () => {
     cy.on('window:alert', (str) => {
       expect(str).to.equal('email or password is wrong');
     });
+  });
+
+  it('should display homepage when email and password are correct', () => {
+    cy.get('input[placeholder="Email"]').type('testariff@gmail.com');
+
+    cy.get('input[placeholder="Password"]').type('qwerty123');
+
+    cy.get('button').contains(/^Login$/).click();
+
+    cy.get('nav').contains(/^Thread$/).should('be.visible');
+    cy.get('button').contains('Log out').should('be.visible');
   });
 });
